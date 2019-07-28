11262754ce797aed03d024bd559a1f3139c7af95
bsihame <sihamebazi@pursuit.org>
2019-07-28 09:57:14 -0400

    lab done

--- web-functions-and-objects-review.js
+++ web-functions-and-objects-review.js
@@ -188,16 +188,16 @@
 // Input: [3,-4,5,-6]
 // Output: 18
 // ```
-let input9 = [3,-4,5,-6];
-
-function sumOfAbsolute(arr) {
-  var result = 0
-  for(var i = 0; i < arr.length; i++){
-    result += Math.abs(arr[i] + arr[i++]);
-  }
-  return result;
-}
-console.log(sumOfAbsolute(input9));
+// let input9 = [3, -4, 5, -6];
+//
+// function sumOfAbsolute(arr) {
+//   var result = 0
+//   for(var i = 0; i < arr.length; i++){
+//     result += Math.abs(arr[i]);
+//   }
+//   return result;
+// }
+// console.log(sumOfAbsolute(input9));
 
 // ## 10. Fahrenheit to Celsius
 //
@@ -207,27 +207,167 @@ console.log(sumOfAbsolute(input9));
 // Input: 212
 // Output: 100
 // ```
-//
+// let temperatureF = 212;
+// const FahrenheitToCelsius = (temp) => {
+//   return (temp - 32) * 5/9;
+// }
+// console.log(FahrenheitToCelsius(temperatureF)+ "°C" );
 // ## Objects
 //
-// 1. Write an object that represents a User.  It should have a property for the user's name, email address, and age.
-//
+// 1. Write an object that represents a User.
+// It should have a property for the user's name,
+//email address, and age.
+
+// let user = {
+//   name : 'Faris',
+//   email : 'bfaris@gmail.com',
+//   age : 11
+// };
 // 2. Make 3 different user objects and store them in an array
+//way1:
+//=====
+let customers = [
+  {
+  name : 'Diana',
+  email : 'diana@hotmail.com',
+  age : 62
+},{
+  name : 'Ryan',
+  email : 'ryan@gmail.com',
+  age : 30
+}, {
+  name : 'Tabz',
+  email : 'tabz@yahoo.com',
+  age : 16
+}
+]
+let arrOfCustomers = customers;
+console.log(arrOfCustomers);
+//==============================
+//way2
+//====
+// let cust1 = {
+//   name : 'Diana',
+//   email : 'diana@hotmail.com',
+//   age : 62};
+//   let cust2 = {
+//   name : 'Ryan',
+//   email : 'ryan@gmail.com',
+//   age : 30};
+//   let cust3 = {
+//   name : 'Tabz',
+//   email : 'tabz@yahoo.com',
+//   age : 16};
+// let arrOfCustomers2 = [cust1, cust2, cust3];
+// console.log(arrOfCustomers2);
+// 3. Write a function that takes in an array of
+//User objects, and returns an array with only their email addresses
+// const emailsFilter = (array) => {
+//   return array.map(object => object.email)
+// }
+// let emailsArray = emailsFilter(arrOfCustomers);
+// console.log(emailsArray);
+// 4. Write a function that takes
+//in an array of User objects,
+//and returns the average age of all users. Rounded down.
 //
-// 3. Write a function that takes in an array of User objects, and returns an array with only their email addresses
+const usersAverageAgeRoundedDown = (array) => {
+  let sum = array.reduce((acc, object) => {
+    //console.log(object.age);//to check
+    return acc += object.age;
+  }, 0)
+  //console.log(`reduce`, sum)//to check
+  return Math.floor(sum / array.length);
+}
+console.log(usersAverageAgeRoundedDown(arrOfCustomers));
+// 5. Write a function that takes in an array of User
+//objects and returns whether or not all
+//users have an age greater than 18
+
+const testAgeGreather18 = (array) => {
+  return array.every(object => {
+    if (object.age > 18) {
+      return true;
+    }
+  })
+}
+
+console.log(testAgeGreather18(arrOfCustomers));
+// 6. Write an object that represents a Recipe.
+// It should have a property for the recipe's
+//ingredients, name, and how long it will take to make.
 //
-// 4. Write a function that takes in an array of User objects, and returns the average age of all users. Rounded down.
+// let salmonRecipe = {
+// name:'Pan-Seared Salmon and Crispy Tomatoes Over Quinoa',
+// ingredients:['5 ounces salmon', '½ cup cooked quinoa', '½ cup halved grapetomatoes',
+// '1½ tablespoons balsamic vinegar', 'Fresh basil, for garnish'],
+// prepTime: 7};
+// console.log(salmonRecipe);
+// 7. Make 3 different recipe objects and store
+//them in an array.
 //
-// 5. Write a function that takes in an array of User objects and returns whether or not all users have an age greater than 18
+let salmonRecipe1 = {
+name:'Pan-Seared Salmon and Crispy Tomatoes Over Quinoa',
+ingredients:['5 ounces salmon',
+            '½ cup cooked quinoa',
+            '½ cup halved grapetomatoes',
+            '1½ tablespoons balsamic vinegar',
+            'Fresh basil, for garnish'],
+prepTimeInMinutes: 7};
+let beanRecipe = {
+name:'Black Bean Avocado Salad',
+ingredients:['1 (15½-ounce) can black beans',
+            '2 avocados, chopped into chunks',
+            '4 ounces feta or cotija cheese',
+            'Salsa',
+            'Lime wedges, for garnish',
+            'Cilantro for garnish',
+            'Mixed greens'],
+prepTimeInMinutes: 3};
+let chikenRecipe = {
+name:'Brown Butter Chicken',
+ingredients:['2 boneless, skinless chicken breasts',
+            'Salt and pepper to taste',
+            '½ cup fresh sage leaves',
+            '2 tablespoons butter'],
+prepTimeInMinutes: 5 };
+let recipesIn10mn = [salmonRecipe1, beanRecipe, chikenRecipe];
+//console.log(recipesIn10mn)
+// 8. Write a function that takes in an array of
+//Recipe objects and returns the recipe that will
+//take the least amount of time to make.
 //
-// 6. Write an object that represents a Recipe.  It should have a property for the recipe's ingredients, name, and how long it will take to make.
-//
-// 7. Make 3 different recipe objects and store them in an array.
-//
-// 8. Write a function that takes in an array of Recipe objects and returns the recipe that will take the least amount of time to make.
-//
+const fastestRecipe = (array) => {
+  let shortestTime = Infinity;
+ array.forEach(recipesIn10mn => {
+    if (recipesIn10mn.prepTimeInMinutes < shortestTime){
+      shortestTime = recipesIn10mn.prepTimeInMinutes;
+    }
+  });
+
+  return array.filter(recipesIn10mn => {
+    return (recipesIn10mn.prepTimeInMinutes === shortestTime)
+  })
+}
+console.log(fastestRecipe(recipesIn10mn));
 // 9. Write a function that takes in an array of Recipe objects and returns an array containing all the ingredients you need to make all of the recipes.
 //
 // Bonus: Remove duplicate ingredients from the final array
 //
 // 10. Write a function that takes in an array of Recipe objects and an array of ingredients and returns all the recipes that you are able to make.
+const recipeIngrediants = (array) => {
+  let ingrediants = array.map(recipesIn10mn => {
+    return recipesIn10mn.ingredients;
+  })
+
+  let outputIngrediants =[];
+  ingrediants.forEach(array => {
+    for (element of array) {
+      if (!outputIngrediants.includes(element)) {
+        outputIngrediants.push(element);
+      }
+    }
+  })
+  return outputIngrediants;
+}
+console.log(`List To Do:` ,recipeIngrediants(recipesIn10mn))
