303e09cacbebccf1ddbe2e57cd2a1b8ae3d928b0
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2019-10-14 20:04:08 -0400

    complete problem 3

--- index.js
+++ index.js
@@ -7,8 +7,19 @@
 
 //PROBLEM 2
 
-const averageOfThreeNum = (num1, num2, num3) => {
-    return (num1 + num2 + num3) / 3;
+// const averageOfThreeNum = (num1, num2, num3) => {
+//     return (num1 + num2 + num3) / 3;
+// }
+
+// console.log(averageOfThreeNum(1,3,5));
+
+//PROBLEM 3
+const averageOfArray = (arr) => {
+    let sum = 0;
+    for(let element of arr) {
+        sum += element;
+    }
+    return sum / arr.length;
 }
 
-console.log(averageOfThreeNum(1,3,5));
+console.log(averageOfArray([1,2,3,4,5,10,10]));
