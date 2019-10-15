d45c7377261d6c5df3ae0fe39b5766f7d6031479
Kevin Brutus <kevinbrutus@pursuit.org>
2019-10-14 21:25:47 -0400

    Finish functions, begin object 3

--- lab_function.js
+++ lab_function.js
@@ -52,3 +52,69 @@ const remvNeg = (arry) =>{
 console.log(remvNeg([4,3,-1,4]))
 
 console.log("Problem 6")
+
+const lowerCase = (arr) => {
+    let newArr = []
+    for(let i = 0; i < arr.length; i++){
+        newArr.push(arr[i].toLowerCase())
+    } 
+    return newArr
+}
+
+console.log(lowerCase(["HELLO", "THERE", "THESE", "ARE", "SOME", "STRINGS"]))
+
+console.log("Problem 7")
+const singleStr = arr => {
+    let newStr = ""
+    for(let i = 0; i < arr.length; i ++){
+        newStr = arr.join(" ")
+    } return newStr
+}
+
+console.log(singleStr(["It’s", "not", "a", "bug.", "It’s", "an", "undocumented", "feature!"]));
+
+console.log("Problem 8")
+
+const randomNum = (num1, num2) => {
+    let num = 0
+    num  = Math.floor((Math.random() * num2) + num1)
+    return num
+}
+console.log(randomNum(1, 7))
+
+console.log("Problem 9")
+ const absoluteV = arr => {
+    let sum = 0 
+    for(let i = 0; i < arr.length; i++){
+        sum += Math.abs(arr[i])
+    } return sum
+ }
+
+ console.log(absoluteV([3,-4,5,-6]))
+
+ console.log("Problem 10")
+const farenToCel = temp => {
+    return (temp - 32) * 5/9
+}
+console.log(farenToCel(212))
+
+console.log("Object")
+console.log("Problem 1")
+let user = {
+
+}
+user.name = "Brutus"
+user.email = "kevinbrutus@pursuit.org"
+user.age = 25
+
+console.table(user)
+
+console.log("Problem 2")
+let userArr = [{}]
+userArr[0]['sex'] = "female"
+userArr[0]['shoe size'] = 9
+userArr[0]['favoite movie'] = "The Lion King"
+
+console.log(userArr)
+
+console.log("Problem 3")
