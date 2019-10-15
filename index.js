f2059520413a20e93b47b9cdd741b72ac4b52349
Farrah Isabelle <farrahrios@pursuit.org>
2019-10-14 22:01:31 -0400

    solving problem 4

--- index.js
+++ index.js
@@ -23,15 +23,34 @@
 
 // Write a function that takes in an array of numbers as input and returns their average
 
-let sumArray = [10, 20, 30, 10, 20]
+// let sumArray = [10, 20, 30, 10, 20]
 
 
-const avgFunc = (values) => {
-    let sum = 0 
-    for (let i = 0; i < values.length; i += 1) {
-        sum += values[i]
-    }
-    let divideSum = sum / values.length 
-    return divideSum 
+// const avgFunc = (values) => {
+//     let sum = 0 
+//     for (let i = 0; i < values.length; i += 1) {
+//         sum += values[i]
+//     }
+//     let divideSum = sum / values.length 
+//     return divideSum 
+// }
+// console.log(avgFunc(sumArray))
+
+let oddArray = [3,4,5,6,7,8]
+let newArray = []
+
+const oddFunction = (values) => {
+
+for (let i = 0; i <= values.length; i++) {
+    if (values[i] % 2 === 0) {
+        
+       
+        console.log(newArray.push(values[i])) // => 4 (length of arr) 
+        
+        
+
+
+    } 
 }
-console.log(avgFunc(sumArray))
+}
+console.log(oddFunction(newArray))
