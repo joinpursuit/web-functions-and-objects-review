9b87c810719366c6b99300a5453fd8bb0779808c
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2019-10-14 20:01:15 -0400

    complete problem 2

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 //PROBLEM 1
-const averageOfTwoNum = (num1, num2) => {
-    return (num1 + num2) / 2;
+// const averageOfTwoNum = (num1, num2) => {
+//     return (num1 + num2) / 2;
+// }
+
+// console.log(averageOfTwoNum(4,6));
+
+//PROBLEM 2
+
+const averageOfThreeNum = (num1, num2, num3) => {
+    return (num1 + num2 + num3) / 3;
 }
 
-console.log(averageOfTwoNum(4,6));
+console.log(averageOfThreeNum(1,3,5));
