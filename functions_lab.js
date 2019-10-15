3b39cd57840206788c316def5c3699ed1ab2f45c
Cassidy Beni <cassidybeni@pursuit.org>
2019-10-14 21:59:52 -0400

    questions 1-4

---
+++ functions_lab.js
@@ -0,0 +1,41 @@
+//Question 1
+const findAverage = (num1, num2) => {
+    let sum = num1 + num2
+    let average = sum/ 2
+   return average
+    }
+   console.log(findAverage(4, 6));
+
+//Question 2
+const avOfThree = (num1, num2, num3) => {
+    let sum = num1 + num2 + num3
+    let average = sum / 3
+    return average
+   }
+   console.log(avOfThree(1, 3, 5));
+
+   //Question 3
+let arr = []
+   const avOfArray = (arr) => {
+      let sum = 0
+      let length = 0
+    for (let i = 0; i < arr.length; i++) {
+     sum += arr[i];
+    }
+      let average = sum / arr.length;
+      return average
+   }
+   console.log(avOfArray([1, 2, 3, 4, 5, 10, 10]))
+
+ // Question 4
+ let i = 0
+ let newArray = []
+const evenFunction = (arr) => {
+ for (let i = 0; i < arr.length; i++) {
+   if (i % 2 !== 0) {
+   arr[i] = newArray.push(i)
+   }
+ }
+ return newArray
+}
+console.log(evenFunction([1,2,3,4,5,6]))
