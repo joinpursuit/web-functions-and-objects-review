29bb8d9650664cd9304b02d6e46b94a2c7302d9d
trilltown <phil.awich@gmail.com>
2019-10-14 20:03:04 -0400

    Solve problem 1

---
+++ index.js
@@ -0,0 +1,21 @@
+// ## 1. Average of two numbers
+
+// Write a function that takes in two numbers as input and returns their average
+
+const averageFunction = (num1, num2) => {
+    let addFunc = num1 + num2;
+    let divideFunc = addFunc/2;
+    return divideFunc 
+}
+console.log(averageFunction(4, 6))
+
+
+
+
+
+
+
+// ```
+// Input: 4,6
+// Output: 5
+// ```
