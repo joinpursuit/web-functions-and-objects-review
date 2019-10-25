4153e888f022b8375cae7f8556efeb4977a4f3cf
DejaLFlynn <dejaflynn@pursuit.org>
2019-10-24 21:22:10 -0400

    lab done minus 1 question

--- functionObjectreview.js
+++ functionObjectreview.js
@@ -61,3 +61,129 @@ const fToC = (num10)=>{
     return (num10 - 32)* 5/9
 }
 console.log(fToC(32))
+console.log('//////////////OBJECTS/////////////')
+console.log('QUESTION 1')
+let user = {
+    name: 'Johnathan',
+    email: 'johnnyappleseed',
+    age: 22
+}
+console.log(user)
+console.log('QUESTION 2')
+let userArray =[
+    {
+        name: 'phone bill',
+        date: 'October',
+        amount: '$40',
+    },
+      {
+        name: 'food',
+        date: 'November',
+        amount: '250',
+
+      },
+      {
+        name: 'gas',
+        date: 'November',
+        amount: '60',
+
+      }, 
+
+]
+
+    console.log(userArray)
+
+
+console.log('QUESTION 3')
+let threeUsers =[{
+    userName: 'Sara',
+    email: 'sara@facebook.com',
+    age: 3,
+},
+{
+    userName: 'Tara',
+    email: 'tara@facebook.com',
+    age: 33,
+},
+{
+    userName: 'Lara',
+    email: 'lara@facebook.com',
+    age: 44,
+},
+]
+for (let i=0; i<threeUsers.length; i++){
+    console.log(threeUsers[i].email)
+}
+
+
+console.log('QUESTION 4')
+for (let i=0; i<threeUsers.length; i++){
+    console.log(threeUsers[i].age)
+}
+
+console.log('QUESTION5')
+let under18 = threeUsers.every(el =>{
+    if(el <18===threeUsers){
+        return true
+    } else {
+        return false;
+    }
+})
+console.log(under18)
+
+console.log('QUESTION 6')
+let recipes = [
+    {
+      ingredients: 'milk, flour, eggs, oil',
+      name: 'gf Pancakes',
+      time: 25,
+    },
+    {
+      ingredients: 'condense milk, vanilla, eggs, sweetened milk, sugar',
+      name: 'flan',
+      time: 60,
+    },
+    {
+        ingredients: 'rice and peas',
+        name: 'rice, gandules, sazon, adobo, olives',
+        time:45,
+    }
+  
+  ];
+  console.log(recipes);
+  console.log('takes 10 to make');
+
+
+  console.log('QUESTION 7')
+  for (let i = 0; i < recipes.length; i++) {
+    console.log(recipes[i].ingredients);
+  }
+
+  console.log('QUESTION 8')
+const quickRecipe = (array)=>{
+    let currtime = array[0].time
+    for(let key in array){
+        if(array[key].time<currtime){
+            currtime =arr[key].time
+        }
+        return currtime
+    }
+}
+console.log(quickRecipe(recipes))
+
+  
+  console.log('QUESTION 9')
+  const items = (array)=>{
+
+  
+  let recipesName = [];
+  let organized = array.map(el=>{
+    for(let i = 0; i <el['ingredients'].length; i++) {
+        
+        recipesName.push(el['ingredients'][i]);
+    }
+  })
+return recipesName.filter((el,cur)=>recipesName.indexOf(el)===el)
+}
+
+console.log(items(recipes))
