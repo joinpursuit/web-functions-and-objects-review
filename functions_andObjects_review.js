005b2450b76f9acc496456fd3d2228aec99de497
Kelvin <kelvinarellano@pursuit.org>
2019-11-05 07:12:44 -0500

    redo 1hr

--- functions_andObjects_review.js
+++ functions_andObjects_review.js
@@ -14,6 +14,7 @@ const averageOfThree = (num1,num2,num3) =>{
 const averageOfAmount = (...args) =>{
     return averageOfArray(args)
 }
+// average
 //console.log(averageOfThree(1,3,5))
 //
 //
@@ -25,6 +26,13 @@ const averageOfArr = arr =>{
    })
    return sum/arr.length
 }
+//using a reduce function
+const averageOfArr2 = arr =>{
+    return arr.reduce((accu, el)=>{
+        return accu+el / arr.length
+    },0)
+}
+//console.log(averageOfArr2([1,3,5]))
 //console.log(averageOfArr([1,3,5]))
 //
 //
@@ -40,3 +48,126 @@ const filterEvenNum = arr =>{
 //
 //
 //5.
+const possitiveCheck = arr =>{
+    return arr.every((el)=>{
+        if(el > 0){}
+    })
+}
+//console.log(possitiveCheck([-4,4,-2,4]))
+//
+//
+//6.
+const lowerCaseStrings = arr =>{
+    return arr.map((el)=>{
+        if(typeof el === "string"){
+            return el.toLowerCase()
+        }
+    })
+}
+//console.log(lowerCaseStrings(["HELLO","THESE","ARE","SOME","STRINGS"]))
+//
+//
+//7.
+const joinStrings = arr =>{
+    let combined = arr.join(" ")
+    return combined
+}
+//console.log(joinStrings(["It's", "not", "a", "bug", "it's", "a", "feature"]))
+//
+//
+//8.
+const RandomBetween = num =>{
+    let random = (Math.floor((Math.random() * num) + 1))
+        return random
+}
+//console.log(RandomBetween(10))
+//
+//
+//9.
+const absoluteSumArr = arr =>{
+    return arr.reduce((el,accu)=>{
+        return el+ Math.abs(accu)
+    }, 0)
+}
+//with forEach
+//
+//
+//console.log(absoluteSumArr([-3,-4,5,-6]))
+//
+//
+//10.
+const farhToCels = temp =>{
+    let newTemp = (temp - 32) * 5/9
+        return newTemp
+}
+//console.log(farhToCels(212))
+//
+//
+//bonus
+//
+//
+//1.
+newUser = {
+name: "Kelvin",
+email: "kelvinarellano@pursuit.org",
+age: 28
+}
+//
+//
+//2.
+allNewUsers = [
+    {
+        name:"John",
+        email:"johnsmith@gmail.com",
+        age: 21
+    },
+    {
+        name:"Jane",
+        email:"janedoe@gmail.com",
+        age:22
+    },
+    {
+        name:"Jose",
+        email:"josegarcia@gmail.com",
+        age:23
+    }
+]
+//
+//
+//3.
+const emailOfUsers = arr =>{
+    return arr.map((el)=>{
+        return el.email
+    })
+}
+//console.log(emailOfUsers(allNewUsers))
+//
+//
+//4.
+const avgAgeOfUsers = arr =>{
+    return arr.reduce((el,accu)=>{
+        return Math.floor((el.age += accu/arr.length))
+    },0)
+}
+const avgAgeOfUsers2 = arr =>{
+    let sum = 0
+    arr.forEach((el)=>{
+        sum += el.age
+    })
+    return Math.floor(sum/ arr.length)
+}
+//console.log(avgAgeOfUsers2(allNewUsers))
+//
+//
+//5.
+const ageCheck = arr =>{
+    return arr.every((el)=>{
+        if(el.age >= 18){
+           return true
+        }
+    })
+}
+//console.log(ageCheck(allNewUsers))
+//
+//
+//6.
