b94868e3301f0d5aa6b8c2b50f82f29773d1aa18
serami12 <stephanieramirez1@pursuit.org>
2019-11-26 13:29:13 -0500

    answered q-1

---
+++ functionObj.js
@@ -0,0 +1,7 @@
+// 1. Average of two numbers
+const averageOfNum = (num1, num2) => {
+    let num = num1 + num2
+    for (let i = num1; i < (num.length) / 2; i++) {}
+    return num
+}
+console.log(averageOfNum(4, 6))
