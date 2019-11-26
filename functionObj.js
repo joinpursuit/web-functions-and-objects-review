0119fc46b06fdde7379bec3d342a9a6c182ce193
serami12 <stephanieramirez1@pursuit.org>
2019-11-26 17:12:42 -0500

    answered question two

--- functionObj.js
+++ functionObj.js
@@ -4,3 +4,11 @@ const averageOfNum = (num1, num2) => {
     return average
 }
 console.log(averageOfNum(4, 6))
+
+//2. Average of three numbers
+
+const avegOfThreeNum = (num1, num2, num3) => {
+    let average = (num1 + num2 + num3) / 3
+    return average
+}
+console.log(avegOfThreeNum(1, 3, 5))
