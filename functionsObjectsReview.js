d2233028aa5d502bd4a6f63d0803daa1d313dee6
Wilsantos1975 <faustosantos@pursuit.org>
2019-12-02 09:58:29 -0500

    revisit lab functions and object

--- functionsObjectsReview.js
+++ functionsObjectsReview.js
@@ -28,13 +28,13 @@
 // }
 // console.log(average(20, 15, 42));
 
-// ## 3. Average of array
+// // ## 3. Average of array
 
-// Write a function that takes in an array of numbers as input and returns their average
-// ```
-// Input: [1,2,3,4,5,10,10]
-// Output: 5
-// ```
+// // Write a function that takes in an array of numbers as input and returns their average
+// // ```
+// // Input: [1,2,3,4,5,10,10]
+// // Output: 5
+// // ```
 // const averageArray = (arr) => {
 //     let sum = 0
 //     for(i = 0; i < arr.length; i++){
@@ -64,6 +64,16 @@
 // }
 // console.log(removeEven([1,2,3,4,5]))
 
+// // another way:
+
+// const noEvens = (arr) => {
+//     return arr.filter(el => {
+//       if(el % 2 === 1)
+//       return el
+//     })
+//   }
+//   console.log(noEvens([1,2,3,4,5,6,7,8,9]))
+
 // ## 5. Make sure each number is positive
 
 // Write a function that checks to see whether every number in an array is positive.
@@ -82,9 +92,43 @@
 // }
 // console.log(checkPositive([2,4,6,8,9,4,0]))
 
+// another way:
+
+// const allPositive = (arr) => {
+//     arr.every (el => {
+//         if(el > 0) {
+//         return true
+//         }
+//     })
+// }
+// console.log(allPositive(-1,2,-33,4,5,6,7,8,9,10))
+
 // ## 6. Lowercase all strings
 
 // Write a function that lowercases all strings in an array of strings.
+
+// const toLowerCase = (arr) => {
+//         arr.map(el => {
+//         if(typeof el === "string") {
+//         return el = el.toLowerCase()
+//         }
+//     })
+//     return arr
+//   }
+// console.log(toLowerCase(["HELLO", "THERE", "THESE", "ARE", "SOME", "STRINGS"]))
+
+// //another way:
+
+// const toLowerCaseOne = (arr) => {
+//     let lowerCaseStr = []
+//     for (let i = 0; i < arr.length; i++) {
+//         if(typeof arr[i] === 'string') {
+//         lowerCaseStr.push(arr[i].toLowerCase()) 
+//         }
+//     }
+//     return lowerCaseStr
+// }
+// console.log(toLowerCaseOne(["HELLO", "THERE", "THESE", "ARE", "SOME", "STRINGS"]))
 
 // ```
 // Input: ["HELLO", "THERE", "THESE", "ARE", "SOME", "STRINGS"]
@@ -95,29 +139,96 @@
 //   - A. If you didn't, rewrite this using an array method with a callback
 
 //   - B. Make it work even if the array has mixed types inside so that input = `[1, 'HELLO', {one: 'uno'}]` would give output = `['hello']` 
-// let newArr = [1, 'HELLO', {one: 'uno'}]
-
-// const lowercaseArray = (arr) => {
-//     let lowercaseStrings = []
-//     for (let i = 0; i < arr.length; i++){
-//        if (typeof arr[i] === "string"){ 
-//            lowercaseStrings.push(arr[i].toLowerCase())
-//        }
-//     }
-//     return lowercaseStrings
-// }
-
-// console.log(lowercaseArray(newArr))
 
 // ## 7. Combine strings
 
 // Write a function that combines an array of strings into a single string separated by spaces.
 
+// const makeASentence = (arr) => {
+//     return arr.join(" ")
+// }
+// console.log(makeASentence(["It’s", "not", "a", "bug.", "It’s", "an", "undocumented", "feature!"]))
+
 // ```
 // Input: ["It’s", "not", "a", "bug.", "It’s", "an", "undocumented", "feature!"]
 // Output: "It’s not a bug. It’s an undocumented feature!""
 // ```
-const makeASentence = (arr) => {
-    return arr.join(" ")
-}
-console.log(makeASentence(["It’s", "not", "a", "bug.", "It’s", "an", "undocumented", "feature!"]))
+
+// ## 8. Random number
+
+// Write a function that returns a random integer between 1 and n
+
+// const randomInteger = (num) => {
+//     return Math.floor(Math.random() * num - 1);
+// }
+// console.log(randomInteger(7))
+
+// // ```
+// // Input: 7
+// // Output: 3
+// // ```
+// // Bonus: Your function should return 1 or 7 inclusive
+
+// // ## 9. Sum of absolute values
+// // Write a function that returns the sum of the absolute values of an array
+
+// const sumAbsolutes = (arr) => {
+//     let sum = 0
+//     arr.forEach(el => {
+//         sum += Math.abs(el)
+//     })
+//     return sum
+// }
+// console.log(sumAbsolutes([3,-4,5,-6]))
+
+// // ```
+// // Input: [3,-4,5,-6]
+// // Output: 18
+
+// // ## 10. Fahrenheit to Celsius
+// // Write a function that converts a temperature in Fahrenheit to Celsius
+
+// const termometer = (num) =>{
+//     // let fahrenheit = num
+//     let celsius = (num - 32) / 1.800 
+//     return celsius
+// }
+// console.log(termometer(212))
+
+// ```
+// Input: 212
+// Output: 100
+
+// // 1. Write an object that represents a User.  
+// It should have a property for the user's name, email address, and age.
+let user = {name: "user", email:"prodigos@hotmail.com", age:40 }
+
+// // 2. Make 3 different user objects and store them in an array
+
+// // 3. Write a function that takes in an array of User objects, and returns an array with only their email addresses
+
+// // 4. Write a function that takes in an array of User objects, and returns the average age of all users. Rounded down.
+
+// // 5. Write a function that takes in an array of User objects and returns whether or not all users have an age greater than 18
+
+// // 6. Write an object that represents a Recipe.  It should have a property for 
+// the recipe's ingredients, name, and how long it will take to make.
+
+// // 7. Make 3 different recipe objects and store them in an array.
+
+// // 8. Write a function that takes in an array of Recipe objects and returns the recipe that will take the least 
+// amount of time to make.
+
+// // 9. Write a function that takes in an array of Recipe objects and returns an array 
+// containing all the ingredients you need to make all of the recipes.
+
+// // Bonus: Remove duplicate ingredients from the final array
+
+// // 10. Write a function that takes in an array of Recipe objects and an array of ingredients 
+// and returns all the recipes that you are able to make.
+
+
+
+
+
+
